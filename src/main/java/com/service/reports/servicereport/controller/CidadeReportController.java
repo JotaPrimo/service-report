ba6b049f434401f2b6b4116dcddf53ad48456ff6
jota santos <gestald118@email.com>
2024-03-09 14:24:27 -0400

    alterado para um unico metodo

--- src/main/java/com/service/reports/servicereport/controller/CidadeReportController.java
+++ src/main/java/com/service/reports/servicereport/controller/CidadeReportController.java
@@ -16,18 +16,30 @@ import java.io.IOException;
 import java.util.Map;
 
 @RestController
-@RequestMapping(ApiPath.CIDADE_PATH)
+@RequestMapping(ApiPath.REPORT_SERVICE)
 public class CidadeReportController {
 
     @Autowired
     private RelatorioService reportService;
 
-    @PostMapping(value = "listAll", produces = MediaType.APPLICATION_PDF_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/listAll", produces = MediaType.APPLICATION_PDF_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<byte[]> generateReport(@RequestBody String jsonData) {
         try {
             // Nome do relatório (pode ser dinâmico com base nos dados se necessário)
             String reportName = "allCidades";
 
+            byte[] reportBytes = reportService.generateReport(reportName, jsonData);
+            return ResponseEntity.ok().body(reportBytes);
+        } catch (IOException | JRException e) {
+            e.printStackTrace(); // Trate a exceção adequadamente em uma aplicação real
+            return ResponseEntity.status(500).build();
+        }
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_PDF_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<byte[]> generateReportV2(@RequestParam String reportName, @RequestBody String jsonData) {
+        try {
+            //String reportName = "allCidades";
             byte[] reportBytes = reportService.generateReport(reportName, jsonData);
             return ResponseEntity.ok().body(reportBytes);
         } catch (IOException | JRException e) {
